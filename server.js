5074a179c9923d1d233a0421cb1740100705b68a
Tung <khuatthanhtung09@gmail.com>
2019-08-14 06:31:35 +0300

    Enable CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const path = require('path');
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
@@ -11,8 +12,20 @@ db.once('open', () => console.log('Connected to Recipe Database'));
 
 app.use(express.json());
 
+app.use(express.static(__dirname));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const recipesRouter = require('./controllers/controller-recipe');
-app.use('/recipes', recipesRouter);
+app.use('/api/recipe', recipesRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}!`));
