5f00aaa20c4d2ac9d85c106dafbc75501a730ae4
Tung <khuatthanhtung09@gmail.com>
2019-08-27 12:38:15 +0300

    Added new actions

--- src/actions/action-recipe.js
+++ src/actions/action-recipe.js
@@ -1,13 +1,54 @@
-const axios = require('axios');
 import { Endpoints } from '../endpoints';
 import { ActionType } from './action-types';
 
-export async function fetchRecipeList (dispatch) {
-  const response = await axios.get(Endpoints.recipeList())
+const axios = require('axios');
+
+export async function fetchRecipeList(dispatch) {
+  const response = await axios.get(Endpoints.recipeList());
   return dispatch(
     {
       type: ActionType.FETCH_RECIPE_LIST,
-      payload: response.data
-    }
+      payload: response.data,
+    },
   );
+}
+
+export async function fetchRecipeItemById(dispatch, id) {
+  const response = await axios.get(Endpoints.recipeById(id));
+  return dispatch(
+    {
+      type: ActionType.FETCH_RECIPE_ITEM,
+      payload: response.data,
+    },
+  );
+}
+
+export async function postNewRecipe(dispatch, newRecipeObject) {
+  const request = axios({
+    method: 'post',
+    url: Endpoints.recipeList(),
+    data: newRecipeObject,
+  });
+  const response = await request;
+
+  return dispatch(
+    {
+      type: ActionType.FETCH_RECIPE_LIST,
+      payload: response.data,
+    },
+  );
+}
+
+export async function deleteRecipeById(dispatch, id, successHandler) {
+  const confirmDelete = confirm('Are you sure you want to delete this recipe?');
+  if (confirmDelete) {
+    const response = await axios.delete(Endpoints.recipeById(id));
+    successHandler && successHandler();
+    return dispatch(
+      {
+        type: ActionType.DELETE_RECIPE,
+        payload: id,
+      },
+    );
+  }
 }
