b5b077387dee0527107c6159f081f58eb0a8457b
Tung <khuatthanhtung09@gmail.com>
2019-08-28 10:00:43 +0300

    Minor changes

--- controllers/controller-recipe.js
+++ controllers/controller-recipe.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 
 
@@ -7,75 +8,88 @@ const Recipe = require('../models/model-recipe');
 // Send GET request to /recipes to READ a list of recipe
 router.get('/', async (req, res) => {
   try {
-    const recipes = await Recipe.find()
+    const recipes = await Recipe.find();
     res.json(recipes);
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
 });
 
 // Send GET request to /recipes/:id to READ a recipe
 router.get('/:id', getRecipeById, (req, res) => {
   res.json(res.recipe);
-})
+});
 
 // Send POST request to /recipes to CREATE a new recipe
 router.post('/', async (req, res) => {
   const recipe = new Recipe({
     name: req.body.name,
-    ingredience: req.body.ingredience,
+    ingredient: req.body.ingredient,
     dateCreated: req.body.dateCreated,
-    steps: req.body.steps,
+    step: req.body.step,
     image: req.body.image,
-    tag: req.body.tag
-  })
+    tag: req.body.tag,
+    description: req.body.description,
+  });
   try {
     const newRecipe = await recipe.save();
     res.status(201).json(newRecipe);
   } catch (error) {
-    res.status(400).json({ message: error.message })
+    res.status(400).json({ message: error.message });
   }
-})
+});
 
 // Send PATCH request to /recipes/:id to UPDATE a recipe
 router.patch('/:id', getRecipeById, async (req, res) => {
   if (req.body.name == null) {
-    res.recipe.name = req.body.name
+    res.recipe.name = req.body.name;
   }
-  if (req.body.ingredience == null) {
-    res.recipe.ingredience = req.body.ingredience
+  if (req.body.ingredient == null) {
+    res.recipe.ingredient = req.body.ingredient;
+  }
+  if (req.body.step == null) {
+    res.recipe.step = req.body.step;
+  }
+  if (req.body.image == null) {
+    res.recipe.image = req.body.image;
+  }
+  if (req.body.tag == null) {
+    res.recipe.tag = req.body.tag;
+  }
+  if (req.body.description == null) {
+    res.recipe.description = req.body.description;
   }
   try {
-    const newRecipe =  await res.recipe.save()
-    res.json(newRecipe)
-  } catch(error){
-    res.status(400).json({ message: error.message })
+    const newRecipe = await res.recipe.save();
+    res.json(newRecipe);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
-})
+});
 
 // Send DELETE request to /recipes/:id to DELETE a recipe
 router.delete('/:id', getRecipeById, async (req, res) => {
   try {
-    await res.recipe.remove()
-    res.json({ message: 'Deleted Recipe' })
+    await res.recipe.remove();
+    res.json({ message: 'Deleted Recipe' });
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
-})
+});
 
 // Helper function to find recipe by id
-async function getRecipeById (req, res, next){
-  let recipe
+async function getRecipeById(req, res, next) {
+  let recipe;
   try {
-    recipe = await Recipe.findById(req.params.id)
+    recipe = await Recipe.findById(req.params.id);
     if (recipe == null) {
-      return res.status(404).json({ message: 'Cannot find recipe' })
+      return res.status(404).json({ message: 'Cannot find recipe' });
     }
   } catch (error) {
-    return res.status(500).json({ message: error.message })
+    return res.status(500).json({ message: error.message });
   }
-  res.recipe = recipe
-  next()
+  res.recipe = recipe;
+  next();
 }
 
 
